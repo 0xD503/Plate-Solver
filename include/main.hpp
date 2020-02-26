421a7b020c418c63b6e12281e5a2b00731c45e7a
0xD503 <ElectricalBrain@protonmail.com>
2020-02-26 23:09:40 +0300

    Added header file

---
+++ include/main.hpp
@@ -0,0 +1,19 @@
+#ifndef MAIN_HPP_INCLUDED
+#define MAIN_HPP_INCLUDED
+
+#include <iostream>
+#include <cerrno>
+
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+
+
+#define KERNEL_SIZE 3
+#define SCALE 1
+#define DELTA 0
+
+
+
+#endif // MAIN_HPP_INCLUDED
