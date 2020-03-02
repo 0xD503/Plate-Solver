e2cca54227e56ce047a89476e2002f7c0e974ed1
0xD503 <ElectricalBrain@protonmail.com>
2020-03-03 02:13:17 +0300

    Create function for enclosing objects with circles

--- include/Star_Detection.hpp
+++ include/Star_Detection.hpp
@@ -19,8 +19,10 @@
 using namespace std;
 using namespace cv;
 
+void encloseWithCircles (vector <vector <Point>> contoursPolygon_vec, vector <Point2f> centers, vector <float> radiuses,
+                         size_t num, Mat starContours_mat, Scalar color, int thickness, int lineType, int shift);
 void encloseWithRectangles (vector <Rect> boundedRects, vector <vector <Point>> contoursPolygon_vec, size_t num,
-                            Mat starContour, Scalar color, int thickness, int lineType, int shift);
+                            Mat starContours_mat, Scalar color, int thickness, int lineType, int shift);
 
 
 #endif  //  __STAR_DETECTION_H__
--- src/Star_Detection.cpp
+++ src/Star_Detection.cpp
@@ -21,7 +21,6 @@ using namespace cv;
     for (i = 0; i < contours.size(); i++)
     {
         approxPolyDP(Mat(contours[i]), vec_Contours_Polygon[i], 3, true);
-        boundedRect[i] = boundingRect(Mat(vec_Contours_Polygon[i]));
         minEnclosingCircle((Mat) vec_Contours_Polygon[i], center[i], radius[i]);
     }
 
@@ -31,18 +30,30 @@ using namespace cv;
     {
         Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
         drawContours(starContours, vec_Contours_Polygon, i, color, 1, 8, vector<Vec4i>(), 0, Point());
-        rectangle(starContours, boundedRect[i].tl(), boundedRect[i].br(), color, 2, 8, 0);
         circle(starContours, center[i], radius[i], color, 2, 8, 0);
     }*/
 
-void encloseWithRectangles (vector <Rect> boundedRects, vector <vector <Point>> contoursPolygon_vec, size_t num,
-                            Mat starContour, Scalar color, int thickness, int lineType, int shift)
+void encloseWithCircles (vector <vector <Point>> contoursPolygon_vec, vector <Point2f> centers, vector <float> radiuses,
+                         size_t num, Mat starContours_mat, Scalar color, int thickness, int lineType, int shift)
 {
+    size_t i;
 
-    for (size_t i = 0; i < num; i++)
+    for (i = 0; i < num; i++)
+    {
+        minEnclosingCircle(Mat(contoursPolygon_vec[i]), centers[i], radiuses[i]);
+        circle(starContours_mat, centers[i], radiuses[i], color, thickness, lineType, shift);
+    }
+}
+
+void encloseWithRectangles (vector <Rect> boundedRects, vector <vector <Point>> contoursPolygon_vec, size_t num,
+                            Mat starContours_mat, Scalar color, int thickness, int lineType, int shift)
+{
+    size_t i;
+
+    for (i = 0; i < num; i++)
     {
         boundedRects[i] = boundingRect(Mat(contoursPolygon_vec[i]));
-        rectangle(starContour, boundedRects[i].tl(), boundedRects[i].br(), color, thickness, lineType, shift);
+        rectangle(starContours_mat, boundedRects[i].tl(), boundedRects[i].br(), color, thickness, lineType, shift);
     }
 }
 
