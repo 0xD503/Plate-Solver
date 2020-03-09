bafeaf79a2063f899ed4947e2a0f2898a47041d0
0xD503 <ElectricalBrain@protonmail.com>
2020-03-09 09:38:57 +0000

    Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+
+##  Installation prequisitions
+#echo 'Installation...'
+echo 'Creating the neccesary directories...'
+mkdir ./obj
+mkdir ./obj/Debug
+mkdir ./obj/Debug/src
+mkdir ./bin
+mkdir ./bin/Debug
+
+##  Compilation
+echo 'Compiling...'
+make
+
+##  Installation
+echo 'Installing'
+
+
+##  Creating run.sh file
+#'./bin/Debug/Plate-Solver\n' > ./run.sh
+
+##
+echo 'Installation status: SUCCESS'
